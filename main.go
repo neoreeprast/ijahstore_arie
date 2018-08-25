77b96604f184dceed749959928ee052a0320187d
Arie Prastowo <you@example.com>
2018-08-25 23:20:31 +0700

    update report to return csv

--- main.go
+++ main.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"bytes"
+	"encoding/csv"
 	"encoding/json"
+	"fmt"
 	"log"
 	"net/http"
+	"strconv"
 
 	"./dao"
 	"./model"
@@ -19,6 +23,7 @@ func CreateProduct(w http.ResponseWriter, r *http.Request) {
 	var product model.Product
 	_ = json.NewDecoder(r.Body).Decode(&product)
 	db.AddProduct(product)
+	w.Header().Add("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(product)
 }
 
@@ -27,6 +32,7 @@ func CreatePO(w http.ResponseWriter, r *http.Request) {
 	var po model.PurchaseOrder
 	_ = json.NewDecoder(r.Body).Decode(&po)
 	db.CreatePO(po)
+	w.Header().Add("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(po)
 }
 
@@ -34,6 +40,7 @@ func ReceiveProduct(w http.ResponseWriter, r *http.Request) {
 	var ir model.ItemReceived
 	_ = json.NewDecoder(r.Body).Decode(&ir)
 	db.InsertReceiptItem(ir)
+	w.Header().Add("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(ir)
 }
 
@@ -41,17 +48,50 @@ func CreateSO(w http.ResponseWriter, r *http.Request) {
 	var so model.SalesOrder
 	_ = json.NewDecoder(r.Body).Decode(&so)
 	db.CreateSalesOrder(so)
+	w.Header().Add("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(so)
 }
 
 func ViewValuationReport(w http.ResponseWriter, r *http.Request) {
 	valuationReport := db.GetValuationReport()
-	json.NewEncoder(w).Encode(valuationReport)
+	b := &bytes.Buffer{}
+	wr := csv.NewWriter(b)
+	for _, vr := range valuationReport {
+		wr.Write([]string{
+			vr.SKU,
+			vr.ItemName,
+			strconv.FormatInt(vr.CurrentStock,10),
+			fmt.Sprintf("%.0f",vr.AverageBuyPrice),
+			fmt.Sprintf("%.0f",vr.Total),
+		})
+	}
+	wr.Flush()
+	w.Header().Set("Content-Type", "text/csv")
+	w.Header().Set("Content-Disposition", "attachment;filename=ValuationReport.csv")
+	w.Write(b.Bytes())
 }
 
 func ViewSalesReport(w http.ResponseWriter, r *http.Request) {
 	salesReport := db.GetSalesReport()
-	json.NewEncoder(w).Encode(salesReport)
+	b := &bytes.Buffer{}
+	wr := csv.NewWriter(b)
+	for _, sr := range salesReport {
+		wr.Write([]string{
+			sr.OrderID,
+			sr.OrderTime.Format("2006-01-02 15:04:05"),
+			sr.SKU,
+			sr.ItemName,
+			strconv.FormatInt(sr.Qty,10),
+			fmt.Sprintf("%.0f",sr.SalePrice),
+			fmt.Sprintf("%.0f",sr.TotalSale),
+			fmt.Sprintf("%.0f",sr.BuyPrice),
+			fmt.Sprintf("%.0f",sr.Revenue),
+		})
+	}
+	wr.Flush()
+	w.Header().Set("Content-Type", "text/csv")
+	w.Header().Set("Content-Disposition", "attachment;filename=SalesReport.csv")
+	w.Write(b.Bytes())
 }
 
 func init() {
